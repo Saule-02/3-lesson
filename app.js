4af9bebffe78ef040a04eed1c5e90c64e5439187
Saule <talantbekkyzysaule6@gmail.com>
2023-05-22 17:58:39 +0600

    done lesson 3

---
+++ app.js
@@ -0,0 +1,98 @@
+// var myAge = 20
+
+
+// var myNameIs = 'Saule'
+
+
+// var isRainy = false 
+
+
+// var mySister = null 
+
+
+// var myDog;
+
+
+// var phone = {
+//     model: 'iPhone X',
+//     color: 'black',
+//     price: 999,
+//     owner:myNameIs  
+//   }
+
+
+
+
+// var color = '' 
+// if (color === 'red') {
+//     console.log('STOP');
+// }
+// else if (color === 'green') {
+//     console.log('GO');
+// }
+// else if (color === 'yellow') {
+//     console.log('WAIT...');
+// }
+// else {console.log('---no color---')
+// }
+
+
+// var operation = '/'
+// if (operation === '+'){
+//     console.log(100 + 50);
+// }
+// else if (operation === '-')  {
+//     console.log(100 - 50);
+// }
+// else if (operation === '*')  {
+//     console.log(100 * 50);
+// }
+// else if (operation === '/')  {
+//     console.log(100 / 50);
+// }
+
+// var temp = 37
+// if (temp > 41) {
+//     console.log('куун ысыыккы...');
+// }
+// else if (temp > 35 && temp > 40) {
+//     console.log('куун ысыыккы...Абдаан ысыыккы...');
+// }
+
+
+
+// var text = prompt ('enter text')
+// console.log(text);
+// if (text.toLocaleLowerCase() === 'saule') {
+//     console.log('yes i am');
+// }
+
+
+// var someText = '5' 
+// switch (someText) {
+//     case 'Monday':
+//         console.log('Понедельник');
+//        break;
+//         case '5':
+//             console.log('Пятница');
+//         break;
+//         case 'Sunday':
+//             console.log('Воскресенье');
+//     default:
+//         break;
+// }
+
+// if (!true) {
+//     console.log('true---');
+// }
+
+
+var userDB = {
+    name: 'Saule',
+    age: 25,
+    email: 'talantbekkyzysaule6@gmail.com',
+    password: 'saule2023'
+}
+console.log(userDB);
+
+
